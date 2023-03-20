0c828a7e36828ce04819243d08b8198fedd95356
Yevhenii <djek195@gmail.com>
2023-03-20 18:40:39 +0200

    lesson 90
    we used async/await for data, wrote new method for create cards in website and deleted old

--- js/script.js
+++ js/script.js
@@ -196,37 +196,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // const div = new MenuCard();
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container",
-    "menu__item",
-    "big"
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    14,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    21,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    return await res.json();
+  };
+
+  getResource("http://localhost:3000/menu").then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
 
   //XMLHttpRequest
 
@@ -241,11 +232,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //12 взяти всі наші форми і підв'язати їх до функції //1 postData()
   forms.forEach((form) => {
-    postData(form);
+    bindPostData(form);
   });
 
+  //lesson 90 async await
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: data,
+    });
+
+    return await res.json();
+  };
+
   //1 функція яка відповідає за постинг данних
-  function postData(form) {
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       //2 submit - спрацьовує кожен раз коли ми пробуємо відправити якусь форму
       e.preventDefault(); // відміняємо стандартну поведінку браузера
@@ -261,17 +263,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      fetch("server.php", {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(object),
-      })
-        .then((data) => data.text())
+      postData("http://localhost:3000/requests", json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
@@ -329,7 +323,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // lesson 89
 
-  fetch("http://localhost:3000/menu")
-    .then((data) => data.json())
-    .then((res) => console.log(res));
+  //   fetch("http://localhost:3000/menu")
+  //     .then((data) => data.json())
+  //     .then((res) => console.log(res));
 });
